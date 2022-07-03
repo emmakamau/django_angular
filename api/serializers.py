cc77fe5cb0da0ed2066391fb3407990275674211
EmmaK <emmaculatewkamau@gmail.com>
2022-07-03 15:24:06 +0300

    Add serializers

---
+++ api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Departments, Employees
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departments
+        fields = ('DepartmentId',
+                  'DepartmentName')
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = ('EmployeeId',
+                  'EmployeeName',
+                  'Department',
+                  'DateOfJoining',
+                  'PhotoFileName')
